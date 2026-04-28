d845f705406c83f880e5b151119b4aec929b9742
Shehraz Ali <Shehrazali365@gmail.com>
2026-04-28 17:40:02 +0500

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ButtonSvg from '../assets/svg/ButtonSvg'
+
+const Button = ({ className, href, onClick, children, px, white }) => {
+
+    const Classes = `button relative inline-flex
+     items-center justify-center h-11 transition-colors
+      hover:text-color-1 ${px || 'px-7'} 
+      ${white ? 'text-n-8' : 'text-n-1'} 
+      ${className || ''}`;
+    const spanClass = `relative z-10`
+
+
+    const renderButton = () => (
+        <button className={Classes} onClick={onClick}>
+            <span className={spanClass}>{children}</span>
+            {ButtonSvg(white)}
+        </button>
+    );
+
+    const renderLink = () => (
+        <a href={href} className={Classes}>
+            <span className={spanClass}>{children}</span>
+            {ButtonSvg(white)}
+        </a>
+    )
+
+    return href ?
+    renderLink() : 
+    renderButton() ;
+}
+
+export default Button
---
+++ src/components/PricingList.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { pricing } from '../constants'
+import Button from './Button'
+import { check } from '../assets'
+
+const PricingList = () => {
+  return (
+    <div className=' flex gap-[1rem] max-lg:flex-wrap'>
+      {pricing.map((item, index) => (
+        <div key={index} id={item.id} className='w-[19rem] max-lg:w-full h-full px-4 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto  even:py-14 odd:py-8'>
+          <h4 className=' h4 mb-4'>{item.title}</h4>
+          <p className=' body-2 min-h-[4rem] mb-3 tet-n-1/50'>{item.description}</p>
+          <div className=' flex items-center h-[5.5rem] mb-6'>
+            {item.price && (
+              <>
+                <div className=' h3'>$</div>
+                <div className=' text-[5.5rem] leading-none font-bold'>{item.price}</div>
+              </>
+            )}
+          </div>
+
+          <Button className={` w-full mb-6`} href={item.price ? '/pricing' : 'mailto:shehrazali360@gmail.com'} white={!!item.price}>
+            {item.price ? 'Get started' : 'Contact US'}
+          </Button>
+
+          <ul>
+            {item.features.map((feature , index) => (
+              <li key={index} className=' flex items-start py-5 border-t border-n-6'>
+                <img src={check} width={24} height={24} alt='check' />
+                <p className=' body-2 ml-2'>{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default PricingList
